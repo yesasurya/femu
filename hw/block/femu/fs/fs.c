8c722bcd0e89aa33379e92fa67c5212c1817e117
yesa <yesa@yesasurya.com>
2020-04-14 11:23:16 +0700

    Temp12

--- hw/block/femu/fs/fs.c
+++ hw/block/femu/fs/fs.c
@@ -1,3 +1,4 @@
+#include "qemu/osdep.h"
 #include "fs.h"
 
 uint64_t fs_get_inode_total(uint64_t mem_size_bytes) {
--- hw/block/femu/fs/fs.h
+++ hw/block/femu/fs/fs.h
@@ -1,6 +1,8 @@
 #ifndef FEMU_FS_H
 #define FEMU_FS_H
 
+#include "qemu/osdep.h"
+
 struct fs_inode {
     uint32_t inode_number;
     uint64_t address;
