c821dbc4a5da1e41a882af375686ff0dcf683f13
yesa <yesa@yesasurya.com>
2020-04-14 10:27:30 +0700

    Temp2

--- hw/block/femu/fs/fs.h
+++ hw/block/femu/fs/fs.h
@@ -1,6 +1,5 @@
 #ifndef FEMU_FS_H
 #define FEMU_FS_H
-#endif
 
 struct fs_inode {
     uint32_t inode_number;
@@ -11,4 +10,4 @@ struct fs_inode_table {
     struct fs_inode* fs_inodes;
 };
 
-uint64_t get_inode_total(uint64_t mem_size_bytes);
+#endif
