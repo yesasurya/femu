d83598aadfb3215a12c0ca95215cc18d4e0c3411
yesa <yesa@yesasurya.com>
2020-05-11 19:01:13 +0700

    Temp

--- hw/block/femu/fs/fs.c
+++ hw/block/femu/fs/fs.c
@@ -22,13 +22,13 @@ uint64_t fs_open_file(struct fs_inode_table *inode_table, char *filename) {
     if (fd != -1)
         return fd;
 
-    struct fs_inode inode = inode_table->inodes[inode_table->num_entries + 1];
-    inode.type = FS_INODE_FILE;
-    inode.filename = filename;
-    inode.number = inode_table->num_entries + 1;
-    inode.address = (SLBA_DATA * 0x200) + (inode.number - 1) * inode_table->max_file_size;
-    inode.length = 0;
+    struct fs_inode *inode = &inode_table->inodes[inode_table->num_entries + 1];
+    inode->type = FS_INODE_FILE;
+    inode->filename = filename;
+    inode->number = inode_table->num_entries + 1;
+    inode->address = (SLBA_DATA * 0x200) + (inode.number - 1) * inode_table->max_file_size;
+    inode->length = 0;
 
     inode_table->num_entries++;
-    return inode.number;
+    return inode->number;
 }
