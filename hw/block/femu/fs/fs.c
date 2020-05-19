fb7020d669e2b3b9a6db440f51d8838bfaca3eb9
yesa <yesa@yesasurya.com>
2020-05-20 00:38:07 +0700

    Test

--- hw/block/femu/fs/fs.c
+++ hw/block/femu/fs/fs.c
@@ -1,5 +1,5 @@
-#include "qemu/osdep.h"
 #include "fs.h"
+#include "../nvme.h"
 
 uint64_t fs_get_inode_total(struct fs_inode_table *inode_table, uint64_t mem_size_bytes) {
     return mem_size_bytes / inode_table->max_file_size;
