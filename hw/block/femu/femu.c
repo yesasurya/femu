0375fd68ab7e56be468e9ce882927da2d620e048
yesa <yesa@yesasurya.com>
2020-06-07 20:56:08 +0700

    Test

--- hw/block/femu/femu.c
+++ hw/block/femu/femu.c
@@ -279,9 +279,13 @@ static int femu_rw_mem_backend_nossd(FemuCtrl *n, NvmeNamespace *ns, NvmeCmd *cm
     void *buf = n->mbe.mem_backend + data_offset;
     bool is_write = (rw->opcode == NVME_CMD_WRITE) ? false : true;
     address_space_rw(&address_space_memory, prp1, MEMTXATTRS_UNSPECIFIED, buf, len, is_write);
-    if (rw->opcode == NVME_CMD_WRITE || rw->opcode == NVME_CMD_READ) {
-        printf("YESA LOG: data_offset = %" PRIu64 "\n", data_offset);
-        printf("YESA LOG: buf = %s\n", (char *)buf);
+    if (rw->opcode == NVME_CMD_WRITE) {
+        printf("YESA LOG: WRITE -> data_offset = %" PRIu64 "\n", data_offset);
+        printf("YESA LOG: WRITE -> buf = %s\n", (char *)buf);
+    }
+    if (rw->opcode == NVME_CMD_READ) {
+        printf("YESA LOG: READ  -> data_offset = %" PRIu64 "\n", data_offset);
+        printf("YESA LOG: READ  -> buf = %s\n", (char *)buf);
     }
 
 //    bool check1 = *(char *)(buf) == 89;
